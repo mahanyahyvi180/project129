1fc445b2201c724349508127458ed1acbd2c06b5
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-22 09:54:05 +0330

    Add files via upload

---
+++ ConsoleApp105/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp105
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal principal = 1000;
+            double rate = 0.05;
+
+            Console.WriteLine("year Amount on deposite");
+
+            for (int year = 1; year <= 10; ++year)
+            {
+                decimal amount = principal * ((decimal)Math.Pow(1.0 + rate, year));
+
+                Console.WriteLine($"{year,4}{amount,20:C}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
